dd93a6ab8af3e5d5ce337239accb8dbc1790310c
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2025-10-17 10:41:50 +0530

    backend-completed

---
+++ backend-go/routes/itemRoutes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/visitha2001/backend-go/handlers"
+)
+
+func RegisterItemRoutes(app *fiber.App, handler *handlers.ItemHandler) {
+	api := app.Group("/items")
+	api.Post("", handler.CreateItem)
+	api.Get("", handler.GetItems)
+	api.Get("/:id", handler.GetItem)
+	api.Get("/total-cost", handler.GetTotalCost)
+	api.Get("/average-cost", handler.GetAvarageCost)
+}
