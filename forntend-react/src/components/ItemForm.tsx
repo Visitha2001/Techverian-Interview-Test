3b421713e012c9410fef7036099e3785b5dae818
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2025-10-18 11:26:58 +0530

    styles

--- forntend-react/src/components/ItemForm.tsx
+++ forntend-react/src/components/ItemForm.tsx
@@ -36,27 +36,29 @@ const ItemForm = ({ onAdded }: ItemFormProps) => {
   }
 
   return (
-    <div className="space-y-8 w-full min-h-[500px] max-h-[500px] bg-gray-800 px-20 py-10 rounded-3xl">
-      <h1 className="text-3xl font-bold text-center">Add Items</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            name="name"
-            control={form.control}
-            label="Item Name"
-            placeholder="Item Name"
-            type="text"
-          />
-          <FormField
-            name="price"
-            control={form.control}
-            label="Price"
-            placeholder="Price"
-            type="number"
-          />
-          <Button type="submit" className="w-full bg-blue-500 hover:bg-blue-600 py-5 rounded-2xl mt-4">Add Item</Button>
-        </form>
-      </Form>
+    <div className="space-y-8 w-full bg-gray-800 rounded-3xl">
+      <div className="sm:px-10 px-6 sm:py-10 py-6">
+        <h1 className="text-3xl font-bold text-center">Add Items</h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              name="name"
+              control={form.control}
+              label="Item Name"
+              placeholder="Item Name"
+              type="text"
+            />
+            <FormField
+              name="price"
+              control={form.control}
+              label="Price"
+              placeholder="Price"
+              type="number"
+            />
+            <Button type="submit" className="w-full bg-blue-500 hover:bg-blue-600 py-5 rounded-2xl mt-4">Add Item</Button>
+          </form>
+        </Form>
+      </div>
     </div>
   )
 }
